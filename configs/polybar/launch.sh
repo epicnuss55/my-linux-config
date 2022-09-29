2d7abec320b9dedd50b475910dd6c2d058fae5e9
Shayan <76680060+epicnuss55@users.noreply.github.com>
2022-09-30 01:42:40 +1000

    Update launch.sh

--- configs/polybar/launch.sh
+++ configs/polybar/launch.sh
@@ -8,6 +8,7 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar topleft | polybar topmiddle | polybar topright | polybar bottomleft | polybar bottommiddle | polybar bottomright
+
 
 echo "Bars launched..."
