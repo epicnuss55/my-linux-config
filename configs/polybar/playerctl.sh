ce12df5c41e51708cbff85e70b8707be3740fd37
Shayan <76680060+epicnuss55@users.noreply.github.com>
2022-12-21 02:22:05 +1100

    new playerctl script

---
+++ configs/polybar/playerctl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+line=`playerctl -l | head -1`
+
+firefox='firefox'
+spotify='spotify'
+
+if [[ "$line" == "$firefox"* ]]; then
+    out=`playerctl metadata --format " {{artist}}: {{title}}"`
+    format="%{u#ff0000}%{+u}"
+    echo $format$out | sed 's/\(.\{85\}\).*/\1.../'
+elif [[ "$line" == "$spotify"* ]]; then
+    out=`playerctl metadata --format " {{artist}}: {{title}}"`
+    format="%{u#1db954}%{+u}"
+    echo $format$out | sed 's/\(.\{85\}\).*/\1.../'
+else
+    out=`playerctl metadata --format " {{artist}}: {{title}}"`
+    format="%{u#87fff1}%{+u}"
+    echo $format$out | sed 's/\(.\{85\}\).*/\1.../'
+fi
+
